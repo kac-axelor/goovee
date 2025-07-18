485e36af70fb2f168695d57a99f29838c4c66870
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-07-18 17:58:47 +0530

    feat: implement completed tasks per month (#98016)

--- constants/index.ts
+++ constants/index.ts
@@ -246,7 +246,7 @@ export const INVOICE_ENTITY_TYPE = {
 export const NO_IMAGE_URL = '/images/no-image.png';
 export const DEFAULT_LOGO_URL = `/images/default-logo.png`;
 
-export const TASK_TYPE_SELECT = {
-  TASK: 'task',
-  TICKET: 'ticket',
-} as const;
+export enum TASK_TYPE_SELECT {
+  TASK = 'task',
+  TICKET = 'ticket',
+}
