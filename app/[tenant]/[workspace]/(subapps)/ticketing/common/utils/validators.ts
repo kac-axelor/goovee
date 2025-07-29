01a0d7b3848812517443b1b3318c85b46eb38613
kac-axelor <215158475+kac-axelor@users.noreply.github.com>
2025-07-29 14:41:16 +0530

    feat: allow same dates for start and end (#98016)

--- app/[tenant]/[workspace]/(subapps)/ticketing/common/utils/validators.ts
+++ app/[tenant]/[workspace]/(subapps)/ticketing/common/utils/validators.ts
@@ -75,7 +75,7 @@ export const FilterSchema = z.object({
         });
       }
       const [startDate, endDate] = [start, end].map(d => new Date(d).getTime());
-      if (startDate >= endDate) {
+      if (startDate > endDate) {
         ctx.addIssue({
           code: z.ZodIssueCode.custom,
           message: 'Start date must be earlier than End date.',
